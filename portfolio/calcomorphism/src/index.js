eaef5c540db99c9d58483557d27834db94a13dbb
sassswing <sassswing@gmail.com>
2021-01-24 09:25:35 +0100

    Corrections mineur calculatrice

--- portfolio/calcomorphism/src/index.js
+++ portfolio/calcomorphism/src/index.js
@@ -277,7 +277,7 @@ function processKey(evt) {
 
     case "op":
 
-      if (lastInputType == "num") {
+      if ((lastInputType == "num") || ((lastInputType == "func") || (lastInputType == "equal")) {
         currentOps = this.getAttribute("data-op");
 
         if ((currentOps == "equal")) { 
